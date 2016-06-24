4014c168ac599220f5238fca9468d4dbc909dfd1
CHOWY <me@chowy.me>
2016-06-24 17:52:33 +0800

    Add (ex-4): Pong Game

---
+++ 04-ponggame.py
@@ -0,0 +1,81 @@
+# http://www.codeskulptor.org/#user41_SbyuwJhwbu_0.py
+
+
+# Implementation of classic arcade game Pong
+
+import simplegui
+import random
+
+# initialize globals - pos and vel encode vertical info for paddles
+WIDTH = 600
+HEIGHT = 400       
+BALL_RADIUS = 20
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH / 2
+HALF_PAD_HEIGHT = PAD_HEIGHT / 2
+LEFT = False
+RIGHT = True
+
+# initialize ball_pos and ball_vel for new bal in middle of table
+# if direction is RIGHT, the ball's velocity is upper right, else upper left
+def spawn_ball(direction):
+    global ball_pos, ball_vel # these are vectors stored as lists
+    ball_pos = [WIDTH / 2, HEIGHT / 2]
+    ball_vel = [-240.0 / 60.0,  30.0 / 60.0]
+
+# define event handlers
+def new_game():
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
+    global score1, score2  # these are ints
+    spawn_ball("RIGHT")
+
+def draw(canvas):
+    global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
+ 
+        
+    # draw mid line and gutters
+    canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
+    canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
+    canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
+        
+    # update ball
+    ball_pos[0] += ball_vel[0]
+    ball_pos[1] += ball_vel[1]
+    
+    # collide and reflect off of left or right hand side of canvas
+    if (ball_pos[0] <= (BALL_RADIUS + PAD_WIDTH)) or (ball_pos[0] >= ((WIDTH - PAD_WIDTH) - BALL_RADIUS)):
+        ball_vel[0] = - ball_vel[0]
+
+    # collide and reflect off of top or bottom of canvas
+    if (ball_pos[1] <= BALL_RADIUS) or (ball_pos[1] >= (HEIGHT - BALL_RADIUS)):
+        ball_vel[1] = - ball_vel[1]
+            
+    # draw ball
+    canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "Red", "White")
+    
+    # update paddle's vertical position, keep paddle on the screen
+    
+    # draw paddles
+    
+    # determine whether paddle and ball collide    
+    
+    # draw scores
+        
+def keydown(key):
+    global paddle1_vel, paddle2_vel
+   
+def keyup(key):
+    global paddle1_vel, paddle2_vel
+
+
+# create frame
+frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
+frame.set_draw_handler(draw)
+frame.set_keydown_handler(keydown)
+frame.set_keyup_handler(keyup)
+
+
+# start frame
+new_game()
+frame.start()
